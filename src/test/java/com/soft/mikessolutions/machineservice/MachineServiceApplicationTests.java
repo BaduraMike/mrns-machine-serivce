e3d4e26bf02be87e8ac2e8273e0dfbc17ec8b530
Michal Badura <badura.mike@gmail.com>
2019-09-09 23:02:53 +0200

    Initial commit.

---
+++ src/test/java/com/soft/mikessolutions/machineservice/MachineServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.soft.mikessolutions.machineservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MachineServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
